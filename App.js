5bd0285247eba9ba2ba06542b091e21c2a216605
Omar Magaña <93545702+MaganaOO06@users.noreply.github.com>
2023-11-14 13:49:52 -0600

    Add files via upload

---
+++ App.js
@@ -0,0 +1,25 @@
+import "./Logo-Guadalupana192.svg";
+import "./App.css";
+import LoginButton from "./components/LoginButton";
+import LogoutButton from "./components/LogoutButton";
+import Profile from "./Profile";
+import { useAuth0 } from "@auth0/auth0-react";
+
+function App() {
+  const { isAuthenticated, isLoading } = useAuth0();
+
+  if (isLoading) {
+    return <h1>Is Loading</h1>
+  }
+
+  return (
+    <div className="App">
+      <h1>La Guadalupana</h1>
+      {isAuthenticated ? <LogoutButton /> : <LoginButton />}
+      <Profile />
+    </div>
+  );
+}
+
+export default App;
+ 
